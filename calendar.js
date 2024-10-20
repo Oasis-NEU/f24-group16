376853ffc8850231b9ba79bd34f3118915112f43
eoinrcollette <eoincollette@gmail.com>
2024-10-20 12:58:19 -0400

    partially displaying dates-javascript

---
+++ calendar.js
@@ -0,0 +1,37 @@
+let calendararray = [
+    [new Date, new Date, new Date, new Date, new Date, new Date, new Date]
+    [new Date, new Date, new Date, new Date, new Date, new Date, new Date]
+    [new Date, new Date, new Date, new Date, new Date, new Date, new Date]
+    [new Date, new Date, new Date, new Date, new Date, new Date, new Date]
+];
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+function displayMonth(x)
+{
+    if()
+}
+
+
+function createCalendar(arr)
+{
+for (let i =0;  i < 3; i ++)
+{
+    for(let x = 0; x < 6; x ++)
+    {
+        if(!isNaN (new Date = (2024, )))
+    }
+}
+}
